464d73853a3d91ceb5e27a8cae850cda0eaea8a6
taehong <hong2wrd@gmail.com>
2026-08-20 09:30:54 +0900

    feat: Getter 추가

--- back/src/main/java/side/todo/domain/BaseEntity.java
+++ back/src/main/java/side/todo/domain/BaseEntity.java
@@ -1,11 +1,13 @@
 package side.todo.domain;
 
 import jakarta.persistence.Column;import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
