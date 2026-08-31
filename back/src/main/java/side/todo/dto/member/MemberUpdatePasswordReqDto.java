52502918ecd0a8a89650ff93c19e2c9358edc1bb
taehong <hong2wrd@gmail.com>
2026-08-31 15:48:38 +0900

    feat: 비밀번호 유효성 체크 추가

--- back/src/main/java/side/todo/dto/member/MemberUpdatePasswordReqDto.java
+++ back/src/main/java/side/todo/dto/member/MemberUpdatePasswordReqDto.java
@@ -1,10 +1,15 @@
 package side.todo.dto.member;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class MemberUpdatePasswordReqDto {
+
+    @NotEmpty
+    @Size(min = 4, max = 20)
     private String changePassword;
 }
